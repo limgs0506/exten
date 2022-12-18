3eb9a682e50b1b6a76343bcb18c0a91a3bfa4245
LimGS <limgs0506@gmail.com>
2022-12-18 18:22:25 +0900

    FIX - 메니페스트 및 content와 service의 통신 방법 수정
    소스 빌드 시 해시값 파일명 때문에scripting.executeScript() 메소드에서 content.js를 지정할 수 없는 문제 발생
    executeScript으로 코드를 주입하는 대신 content에게 message로 정보를 받는 방식으로 변경

--- src/service.ts
+++ src/service.ts
@@ -1,78 +1,9 @@
-chrome.action.onClicked.addListener((tab) => {
-	console.log("트위터 이미지 다운로더 실행됨12");
-	chrome.scripting.executeScript(
-		{
-			target: {
-				tabId: tab.id,
-			},
-			files: ["content.js"],
-		},
-		(result) => {
-			imgObj = result[0].result;
-
-			for (let i = 0; i < imgObj.imgName.length; i++) {
-				chrome.downloads.download({
-					url: imgObj.imgUrl[i],
-					filename: imgObj.username + "/" + imgObj.imgName[i],
-				});
-			}
-		}
-	);
+chrome.action.onClicked.addListener(() => {
+	chrome.tabs.query({ currentWindow: true, active: true }, (result) => {
+		const currentTabID = result[0].id as number;
+		//확장을 사용한 탭의 ID get
+		chrome.tabs.sendMessage(currentTabID, "testMessage", (response) => {
+			console.log(response);
+		});
+	});
 });
-
-/* 
-chrome.action.onClicked.addListener((tab) => {
-	for(url in imgOrig) {
-		chrome.downloads.download({
-			url: url,
-			filename: ,
-			saveAs: false//true로 하면 실행 안됨
-		});
-	}
-}) 
-*/
-
-/* try {
-	chrome.contextMenus.create({
-		"title": "Tweet Image",
-		"id": "1st menu",
-		"type": "normal",
-		"contexts": ["image"]
-	});
-
-	function getIdFromUrl(url) {
-		const idBefore = url.match(/([A-Z])\w+/gi);
-		let idAfter = "@";
-		idAfter += idBefore[3];
-		return idAfter;
-	}
-
-	function getOriginImage(url) {
-		const newUrl = url.replace(/(?<=name=)\w+/gi, "orig");
-		return newUrl;
-	}
-
-	function getFileFormat(url) {
-		const fileFormat = url.match(/(?<=format=)\w+/gi);
-		return fileFormat;
-	}
-
-	chrome.contextMenus.onClicked.addListener((info, tab) => {
-		let idFromUrl = getIdFromUrl((info.linkUrl ?? tab.url));
-		const originImage = getOriginImage(info.srcUrl);
-		//const 날짜;
-		const fileFormat = getFileFormat(originImage);
-		//idFromUrl += 날짜;
-		idFromUrl += ".";
-		idFromUrl += fileFormat;
-
-		console.log(fileFormat);
-		chrome.downloads.download({
-			url: originImage,
-			filename: idFromUrl,
-			saveAs: false//true로 하면 실행 안됨
-		});
-	});
-} catch (e) {
-	console.log(e);
-} */
