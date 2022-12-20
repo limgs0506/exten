dd3fb3b80b28429fcebce9f3d8cca06c1ba644a9
LimGS <limgs0506@gmail.com>
2022-12-20 19:36:21 +0900

    FEAT -  DOM에서 데이터 추출 기능 구현
    content.ts에서 최상위 article의 트윗 작성자 ID, 게시 일자,  이미지 src 추출해 한 인스턴스에 정리

--- src/content.ts
+++ src/content.ts
@@ -1,5 +1,63 @@
+/**
+ * 트윗 아티클 단위로 정보 취합?
+ * 스레드 내 트윗 작성 일시 단위로 취합하는 게 좋을까?(번호 붙이기)
+ */
+class TweetArticle {
+	author: string;
+	Date: string;
+	src: string[];
+	//format: string[];
+
+	constructor(
+		author: string,
+		Date: string,
+		src: string[] /* , format: string[] */
+	) {
+		this.author = author;
+		this.Date = Date;
+		this.src = src;
+		//this.format = format;
+	}
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	if (message == "testMessage") {
-		sendResponse("message recieved");
+	if (message == "download the article img") {
+		const article: HTMLElement = document.querySelector(
+			"article"
+		) as HTMLElement;
+		const textContents = article.querySelectorAll("a");
+		//textContent와 innerText의 차이?
+
+		const author = textContents[2].textContent as string; //아이디
+
+		let date = textContents[4].textContent as string; //날짜
+		let temp = date.match(/[\d]+/g) as RegExpMatchArray;
+		const TEMP_LENGTH = 5;
+		temp[2] = temp[2].slice(2, 4);
+		let tempTwo: string[] = [];
+		tempTwo.push(temp[2]);
+		for (let i = 3; i < TEMP_LENGTH; i++) {
+			if (temp[i].length < 2) {
+				temp[i] = "0" + temp[i];
+			}
+			tempTwo.push(temp[i]);
+		}
+		const dateText = tempTwo.join("");
+
+		const imgList: NodeListOf<HTMLImageElement> =
+			article.querySelectorAll('img[alt="이미지"]');
+		let imgSrcArr: string[] = [];
+		//let imgFormatArr: string[] = [];
+		for (let i = 0; i < imgList.length; i++) {
+			const imgSrc = imgList[i].src;
+			const originImg: string = imgSrc.replace(/(?<=name=)\w+/gi, "orig");
+			imgSrcArr.push(originImg);
+			//imgSrc.match(/(?<=format=)\w+/gi);
+		}
+
+		const tweet = new TweetArticle(author, dateText, imgSrcArr);
+		//article id 검증 파트
+		//각 article에서 이미지 src 추출 파트
+		sendResponse(tweet);
 	}
 });
--- src/service.ts
+++ src/service.ts
@@ -1,9 +1,18 @@
 chrome.action.onClicked.addListener(() => {
 	chrome.tabs.query({ currentWindow: true, active: true }, (result) => {
-		const currentTabID = result[0].id as number;
 		//확장을 사용한 탭의 ID get
-		chrome.tabs.sendMessage(currentTabID, "testMessage", (response) => {
+		const currentTabID = result[0].id as number;
+		const msg = { down: "download the article img" };
+		chrome.tabs.sendMessage(currentTabID, msg.down, (response) => {
 			console.log(response);
+			/* imgObj = result[0].result;
+
+			for (let i = 0; i < imgObj.imgName.length; i++) {
+				chrome.downloads.download({
+					url: imgObj.imgUrl[i],
+					filename: imgObj.username + "/" + imgObj.imgName[i],
+				});
+			} */
 		});
 	});
 });
