91a34d825c96f18f0bb8e75e89b33b1999df1e44
LimGS <limgs0506@gmail.com>
2022-12-21 19:24:17 +0900

    FIX - 불필요한 log, 주석 제거

--- src/content.ts
+++ src/content.ts
@@ -1,47 +1,31 @@
-/**
- * 트윗 아티클 단위로 정보 취합?
- * 스레드 내 트윗 작성 일시 단위로 취합하는 게 좋을까?(번호 붙이기)
- */
 class TweetArticle {
 	author: string;
 	date: string;
 	src: string[];
-	//format: string[];
 
-	constructor(
-		author: string,
-		Date: string,
-		src: string[] /* , format: string[] */
-	) {
+	constructor(author: string, Date: string, src: string[]) {
 		this.author = author;
 		this.date = Date;
 		this.src = src;
-		//this.format = format;
 	}
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	if (message == "download the article img") {
-		console.log("message recieved");
-
 		const article: HTMLElement = document.querySelector(
 			"article"
 		) as HTMLElement;
 		const textContents = article.querySelectorAll("a");
-		//textContent와 innerText의 차이?
 
 		const author =
 			textContents[2].textContent[0] === "@"
 				? (textContents[2].textContent as string)
-				: (textContents[3].textContent as string); //아이디
-		console.log(author);
+				: (textContents[3].textContent as string);
 
 		const date: string = article.querySelector("time").textContent as string;
-		console.log(date);
 
 		let temp = date.match(/[\d]+/g) as RegExpMatchArray;
 		const TEMP_LENGTH = 5;
-		console.log(temp);
 
 		temp[2] = temp[2].slice(2, 4);
 		let tempTwo: string[] = [];
@@ -53,24 +37,19 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 			tempTwo.push(temp[i]);
 		}
 		const dateText = tempTwo.join("");
-		console.log(dateText);
 
 		const imgList: NodeListOf<HTMLImageElement> =
 			article.querySelectorAll('img[alt="이미지"]');
 		let imgSrcArr: string[] = [];
-		//let imgFormatArr: string[] = [];
 		for (let i = 0; i < imgList.length; i++) {
 			const imgSrc = imgList[i].src;
 			const originImg: string = imgSrc.replace(/(?<=name=)\w+/gi, "orig");
 			imgSrcArr.push(originImg);
-			//imgSrc.match(/(?<=format=)\w+/gi);
 		}
 
 		const tweet = new TweetArticle(author, dateText, imgSrcArr);
 		console.log(tweet);
 
-		//article id 검증 파트
-		//각 article에서 이미지 src 추출 파트
 		sendResponse(tweet);
 	}
 });
--- src/service.ts
+++ src/service.ts
@@ -1,6 +1,5 @@
 chrome.action.onClicked.addListener(() => {
 	chrome.tabs.query({ currentWindow: true, active: true }, (result) => {
-		//확장을 사용한 탭의 ID get
 		const currentTabID = result[0].id as number;
 		const msg = { down: "download the article img" };
 		chrome.tabs.sendMessage(currentTabID, msg.down, (response) => {
@@ -18,9 +17,6 @@ chrome.action.onClicked.addListener(() => {
 						if (fileFormat[1] === "jpeg") {
 							fileFormat[1] = "jpg";
 						}
-						//1. jpg => jfif 문제 해결
-						//2. 복수 이미지를 2장까지 밖에 인식하지 못하는 문제 해결
-						//3. 이름 끝에 번호가 안붙는 문제 해결
 						console.log(fileFormat[1]);
 
 						suggest({
