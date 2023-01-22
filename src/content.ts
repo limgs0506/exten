d7d1dbeb251843505141da8f06877444a6f190c5
LimGS <limgs0506@gmail.com>
2023-01-22 18:57:51 +0900

    CHERRY - 9837f656 체리픽

--- src/content.ts
+++ src/content.ts
@@ -1,63 +1,93 @@
-class TweetArticle {
-	author: string;
-	date: string;
-	src: string[];
-
-	constructor(author: string, Date: string, src: string[]) {
-		this.author = author;
-		this.date = Date;
-		this.src = src;
-	}
-}
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	if (message == "download the article img") {
-		const article: HTMLElement = document.querySelector(
-			"article"
-		) as HTMLElement;
-		const textContents = article.querySelectorAll("a");
+	//text author 취득
+	function getAuthor(article: HTMLElement): string {
+		const anchorList: NodeListOf<HTMLAnchorElement> =
+			article.querySelectorAll("a");
 
-		const author =
-			textContents[2].textContent[0] === "@"
-				? (textContents[2].textContent as string)
-				: (textContents[3].textContent as string);
+		for (const anchor of anchorList) {
+			const anchorText: string | null = anchor.textContent;
 
-		function timeFiltering(date: string): string {
-			let temp = date.match(/[\d]+/g) as RegExpMatchArray;
-			let tempTwo: string[] = [];
-			const TEMP_LENGTH = 5;
-
-			temp[2] = temp[2].slice(2, 4);
-			tempTwo.push(temp[2]);
-			for (let i = 3; i < TEMP_LENGTH; i++) {
-				if (temp[i].length < 2) {
-					temp[i] = "0" + temp[i];
-				}
-				tempTwo.push(temp[i]);
+			if (!anchorText) {
+				continue;
 			}
 
-			const dateText = tempTwo.join("");
-			return dateText;
+			if (anchorText[0] === "@") {
+				return anchorText;
+			}
+		}
+		return "UsernameNotFound";
+	}
+
+	//text datetext 취득
+	function getDate(article: HTMLElement): string {
+		interface dateObj {
+			year: string;
+			month: string;
+			day: string;
 		}
 
-		const time: NodeList = article.querySelectorAll("time");
-		const date: string =
-			time.length === 1 ? time[0].textContent : time[1].textContent;
-		const dateText = timeFiltering(date);
+		const date: string | null | undefined =
+			article.querySelector("time")?.textContent;
+		if (!date) {
+			return "error";
+		}
 
+		const dateArr = date.match(/[\d]+/g);
+		if (!dateArr) {
+			return "error";
+		}
+
+		const dateObj: dateObj = {
+			year: dateArr[2].slice(2, 4),
+			month: dateArr[3].length === 2 ? dateArr[3] : "0" + dateArr[3],
+			day: dateArr[4].length === 2 ? dateArr[4] : "0" + dateArr[4],
+		};
+
+		const dateText = dateObj.year + dateObj.month + dateObj.day;
+		return dateText;
+	}
+
+	//arti img.src 취득
+	function getImgSrc(article: HTMLElement): string[] {
 		const imgList: NodeListOf<HTMLImageElement> = article.querySelectorAll(
 			'img[src*="/media/"]'
 		);
 		let imgSrcArr: string[] = [];
-		for (let i = 0; i < imgList.length; i++) {
-			const imgSrc = imgList[i].src;
+
+		if (!imgList.length) {
+			return [];
+		}
+
+		for (const img of imgList) {
+			const imgSrc: string = img.src;
 			const originImg: string = imgSrc.replace(/(?<=name=)\w+/gi, "orig");
+
 			imgSrcArr.push(originImg);
 		}
 
-		const tweet = new TweetArticle(author, dateText, imgSrcArr);
-		console.log(tweet);
+		return imgSrcArr;
+	}
 
-		sendResponse(tweet);
+	if (message == "download the article img") {
+		interface tweet {
+			author: string;
+			date: string;
+			src: string[];
+		}
+
+		const article: HTMLElement | null = document.querySelector("article");
+
+		if (article instanceof HTMLElement) {
+			const tweet: tweet = {
+				author: getAuthor(article),
+				date: getDate(article),
+				src: getImgSrc(article),
+			};
+			console.log(tweet);
+
+			sendResponse(tweet);
+		} else {
+			console.error("constance article is not HTMLElemnet");
+		}
 	}
 });
