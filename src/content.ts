dc641175c8a5ebf3157c60d4ea28f8fb1b1ea7b7
LimGS <limgs0506@gmail.com>
2023-01-25 21:48:06 +0900

    REFAC - 머지 충돌 정리 중 실수로 추가 된 빈 줄 수정

--- src/content.ts
+++ src/content.ts
@@ -47,7 +47,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 			day: dateArr[4].length === 2 ? dateArr[4] : "0" + dateArr[4],
 		};
 
-
 		const dateText = dateObj.year + dateObj.month + dateObj.day;
 		return dateText;
 	}
